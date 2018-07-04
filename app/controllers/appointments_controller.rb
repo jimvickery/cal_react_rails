66d6f3cd834c7e146aaed5796424863ad11e71f0
Jim Vickery <jimrv8@yahoo.com>
2018-07-04 01:43:15 -0500

    added delete button on index with w/o pg reload.

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,4 +1,7 @@
 class AppointmentsController < ApplicationController
+  before_action :find_appointment, only:[:update, :destroy]
+
+
   def index
     @appointments = Appointment.all.order('appt_time ASC')
     @appointment = Appointment.new
@@ -9,11 +12,20 @@ class AppointmentsController < ApplicationController
     redirect_to :root
   end
 
+  def destroy 
+   @appointment.destroy
+   redirect_to :root
+  end
+
 
   private
 
   def appointment_params
     params.require(:appointment).permit(:title, :appt_time)
+  end
+
+  def find_appointment
+    @appointment = Appointment.find(params[:id])
   end
 
 end
