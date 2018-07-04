b1331c092042c6aed912c3ddb2717464564f669f
Jim Vickery <jimrv8@yahoo.com>
2018-07-04 00:46:13 -0500

    added code to submit appt.

--- Gemfile
+++ Gemfile
@@ -19,6 +19,8 @@ gem 'uglifier', '>= 1.3.0'
 gem 'haml', '~> 5.0', '>= 5.0.4'
 gem 'react-rails'
 
+gem 'simple_form', '~> 4.0', '>= 4.0.1'
+
 # Use CoffeeScript for .coffee assets and views
 gem 'coffee-rails', '~> 4.2'
 # Turbolinks makes navigating your web application faster. Read more: https://github.com/turbolinks/turbolinks
--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -3,4 +3,17 @@ class AppointmentsController < ApplicationController
     @appointments = Appointment.all.order('appt_time ASC')
     @appointment = Appointment.new
   end
+
+  def create 
+    @appointment = Appointment.create(appointment_params)
+    redirect_to :root
+  end
+
+
+  private
+
+  def appointment_params
+    params.require(:appointment).perimt(:title, :appt_time)
+  end
+
 end
