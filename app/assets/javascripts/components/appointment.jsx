02e2988bfc1912e8d67388c2034117d49bf13610
Jim Vickery <jimrv8@yahoo.com>
2018-07-05 02:52:14 -0500

    save for now

--- app/assets/javascripts/components/appointment.jsx
+++ app/assets/javascripts/components/appointment.jsx
@@ -1,14 +1,11 @@
-var Appointments = createReactClass({
+var Appointment = createReactClass({
   render: function() {
   return (  
    <div>
      <AppointmentForm />
      {this.props.appointments.map(function(appointment) {
           return (
-            <div>
-              <h3>{appointment.title}</h3>
-              <p>{appointment.appt_time}</p>
-            </div>
+              <Appointment appointment={appointment} />
           )
         })}
    </div>
